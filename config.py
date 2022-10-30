146d078a6e209ddcd5480acee44b5b7dcb8ea766
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2022-10-30 11:32:46 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5620374374:AAFYDge2vy7RMNsGsnJJP7d6qnba8tHV6Go")
     
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 18000552))
     
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("4fc429f67c0543a6f24134cc35851dd4")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
@@ -24,10 +24,10 @@ class Config(object):
     
     PROCESS_MAX_TIMEOUT = 3600
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1647611573"))
 
-    SESSION_NAME = "UploadLinkToFileBot"
+    SESSION_NAME = "urluploadjoebot"
     
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://uploadbotv2:Ichbinschwul2@cluster0.6eyjorl.mongodb.net/?retryWrites=true&w=majority")
 
     MAX_RESULTS = "50"
